55b09812f9f3544ec190f5e5aa39f50a5246ab62
Dilan Lopez <dilanlopez009@gmail.com>
2023-02-11 11:13:15 -0300

    methods http

--- ApiRestFull/server.js
+++ ApiRestFull/server.js
@@ -1,7 +1,24 @@
 import http from 'node:http' // <-- modulo interno do node diferenciado pelo node:
 
 // criação do servidor
+
+const users = []
 const server = http.createServer((req, res) => {
+  const { method, url } = req
+
+  if (method === 'GET' && url === '/users') {
+    return res.end('Usuario listado')
+  }
+
+  if (method === 'POST' && url === '/users') {
+    users.push({
+      id: 1,
+      name: 'John doe',
+      email: 'john.doe@gmail.com'
+    })
+    return res.end('Usuario listado')
+  }
+
   return res.end('Hello world')
 })
 server.listen(3333, () => {
