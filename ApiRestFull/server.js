da6fa6b6b061410807da1bde4a40b000aa367db2
Dilan Lopez <dilanlopez009@gmail.com>
2023-02-11 11:23:03 -0300

    simple get and post sistem

--- ApiRestFull/server.js
+++ ApiRestFull/server.js
@@ -7,7 +7,10 @@ const server = http.createServer((req, res) => {
   const { method, url } = req
 
   if (method === 'GET' && url === '/users') {
-    return res.end('Usuario listado')
+    // retorna array de users em formato json
+    return res
+      .setHeader('Content-Type', 'application/json')
+      .end(JSON.stringify(users))
   }
 
   if (method === 'POST' && url === '/users') {
@@ -16,10 +19,10 @@ const server = http.createServer((req, res) => {
       name: 'John doe',
       email: 'john.doe@gmail.com'
     })
-    return res.end('Usuario listado')
+    return res.writeHead(201).end('Usuario listado')
   }
 
-  return res.end('Hello world')
+  return res.writeHead(404).end('Not Found')
 })
 server.listen(3333, () => {
   console.log('Server running')
