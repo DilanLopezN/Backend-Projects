9e723bf6f4a9b51d6143514b8c5017fdbe665fe6
Dilan Lopez <dilanlopez009@gmail.com>
2023-02-13 17:21:35 -0300

    list transaction test

--- ApiRestFull/src/test/transaction.spec.ts
+++ ApiRestFull/src/test/transaction.spec.ts
@@ -1,19 +1,45 @@
-import { expect, test, beforeAll, afterAll } from 'vitest'
+import { it, beforeAll, afterAll, describe, expect } from 'vitest'
 import request from 'supertest'
 import { app } from '../app'
-beforeAll(async () => {
-  await app.ready()
-})
-afterAll(async () => {
-  await app.close()
-})
-test('o usuário consegue criar uma nova transação', async () => {
-  const response = await request(app.server)
-    .post('/transactions')
-    .send({
-      title: 'New transaction',
-      amount: 500,
-      type: 'credit'
-    })
-    .expect(201)
+
+describe('Transactions routes', () => {
+  beforeAll(async () => {
+    await app.ready()
+  })
+  afterAll(async () => {
+    await app.close()
+  })
+  it('should be able to create a new transaction', async () => {
+    await request(app.server)
+      .post('/transactions')
+      .send({
+        title: 'New transaction',
+        amount: 500,
+        type: 'credit'
+      })
+      .expect(201)
+  })
+
+  it('should be able to list all transaction', async () => {
+    const createTransactionResponse = await request(app.server)
+      .post('/transactions')
+      .send({
+        title: 'New transaction',
+        amount: 500,
+        type: 'credit'
+      })
+    const cookies = createTransactionResponse.get('Set-Cookie')
+
+    const listTransactionResponse = await request(app.server)
+      .get('/transactions')
+      .set('Cookie', cookies)
+      .expect(200)
+
+    expect(listTransactionResponse.body).toEqual([
+      expect.objectContaining({
+        title: 'New transaction',
+        amount: 500
+      })
+    ])
+  })
 })
