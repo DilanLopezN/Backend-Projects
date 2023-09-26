56f32b237141c28eee803b67168892fdebae4c0b
Dilan lopez <dilanlopez009@gmail.com>
2023-09-26 15:16:49 -0300

    jwt middleware

--- solidapi/src/http/controllers/profile.controller.ts
+++ solidapi/src/http/controllers/profile.controller.ts
@@ -1,7 +1,24 @@
+import { makeGetUserProfileService } from "@/services/factories/make.userprofile.service"
 import { FastifyReply, FastifyRequest } from "fastify"
 
 
 export async function profile(request: FastifyRequest, reply: FastifyReply) {
+try {
 
-    
+  const getUserProfile = makeGetUserProfileService()
+
+  const {user} = await getUserProfile.handler({
+    userId: request.user.sub
+  })
+
+  reply.send({
+    user: {
+      ...user,
+      password_hash: undefined
+    }
+  })
+} catch (error) {
+  reply.send(error)
+}
+  
 }
